18c76713b7c925d39afd4dcf9cab8b8ff190f400
Gabriel cavalari dos Santos <gabrielcavalari21@gmail.com>
2026-03-16 20:15:38 -0300

    Exercício 7
    Escreva um programa que calcule a área de um retângulo.

---
+++ exercicio 7.py
@@ -0,0 +1,9 @@
+
+
+base_retangulo= float(input("Qual é medida da base: "))
+altura_retangulo= float(input("Qual é a altura: "))
+
+retan= base_retangulo*altura_retangulo
+print()
+
+print (f"A aréa do retangulo é {retan}")
