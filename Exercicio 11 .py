511aaa6c48a81b12fa856a40ed97adfcf39ff4b2
Gabriel cavalari dos Santos <gabrielcavalari21@gmail.com>
2026-03-22 21:25:43 -0300

    Exercício 11
    Verifique se a pessoa tem idade suficiente para votar.

---
+++ Exercicio 11 .py
@@ -0,0 +1,12 @@
+idade_votar= int(input("Quantos anos voce tem:  "))
+
+print()
+
+if idade_votar<18:
+  
+   print(f" Infelizmente voce tem {idade_votar} anos voce é menor de idade para votar")
+  
+elif idade_votar>=18:
+      
+  print(f"Parabens!Voce ja tem {idade_votar} anos agora ja pode votar")
+      
