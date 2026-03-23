e5dcf65b9107857491aeb8b4ec2b88bad7b0cc5d
Gabriel cavalari dos Santos <gabrielcavalari21@gmail.com>
2026-03-22 21:24:40 -0300

    Exercício 10
    Verifique se um número é par ou ímpar.

---
+++ Exercicio 10 .py
@@ -0,0 +1,15 @@
+conf_numero= int(input("Digite um numero:  "))
+
+calculo= conf_numero%2
+
+print()
+
+
+if calculo==0:
+   
+  print(f"Se sobrar {calculo} é PAR")
+
+elif calculo >0:
+  
+  print(f"Se sobrar {calculo} é IMPAR")  
+      		
