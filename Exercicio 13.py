363c6acf80c6d2944c852461b165b679fb49abaa
Gabriel cavalari dos Santos <gabrielcavalari21@gmail.com>
2026-03-22 21:26:41 -0300

    Exercício 13
    Verifique se um número é positivo, negativo ou zero.

---
+++ Exercicio 13.py
@@ -0,0 +1,14 @@
+
+numero= int(input("Digite um numero: "))
+
+if numero<0:
+
+    print(f" Então esse é um numero negativo {numero}")
+
+elif numero==0:
+
+    print (f"Então esse numero é {numero}")    
+
+elif numero>0:
+
+    print(f"Então esse numero é positivo {numero}")   
