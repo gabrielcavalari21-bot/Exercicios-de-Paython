8933c61489284cba10eaead121e109d97c807a86
Gabriel cavalari dos Santos <gabrielcavalari21@gmail.com>
2026-03-22 21:26:10 -0300

    Exercício 12
    Encontre o maior de dois números.

---
+++ Exercicio 12 .py
@@ -0,0 +1,15 @@
+numero_1= int(input("Digite um numero: "))
+
+print()
+
+numero_2= int(input("Digite um numero: "))
+
+print()
+
+if numero_1>numero_2:
+  
+   print(f"O maior numero é {numero_1}")
+    
+else: 
+  
+  print(f"O maior numero é esse {numero_2}")
