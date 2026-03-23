9584be3f3c0892dd7ff53d7d3753f75d797c31fd
Gabriel cavalari dos Santos <gabrielcavalari21@gmail.com>
2026-03-22 21:12:36 -0300

    Exercício 8
    Escreva um programa que calcule a área de um círculo.

---
+++ Exercicio 8 .py
@@ -0,0 +1,18 @@
+import math
+
+
+
+
+
+raio_circulo= float(input("Digite o valor do Raio: "))
+
+
+
+calculo= math.pi*(raio_circulo**2)
+
+
+print()
+
+
+
+print (f"A area do circulo é {calculo:.2f}"  )
