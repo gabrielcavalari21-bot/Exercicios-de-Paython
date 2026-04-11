aee0d0c00856aba77484eaf6b935081d559a3595
Gabriel cavalari dos Santos <gabrielcavalari21@gmail.com>
2026-04-11 07:04:24 -0300

    Exercício 21
    Conte quantas vezes um número aparece em uma lista.

---
+++ Exercicio 21 .py
@@ -0,0 +1,10 @@
+
+lista=  [1,2,3,1,1,4,5,6,8,9,1,1]
+
+print (f" O numero 1 a parece quantas vezes nessa lista {lista.count(1)} vezes")
+
+frutas= ["banana","pera", "maçã","banana","uva"]
+
+print (f" Quantas bananas tem nessa lista {frutas.count("banana")}")
+
+
