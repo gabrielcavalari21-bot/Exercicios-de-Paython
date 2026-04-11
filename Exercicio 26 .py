710edcac32b85e65185c7fd9c8e2286d7f6e897a
Gabriel cavalari dos Santos <gabrielcavalari21@gmail.com>
2026-04-11 07:08:53 -0300

    Exercício 26
    Escreva uma função que some dois números.

---
+++ Exercicio 26 .py
@@ -0,0 +1,24 @@
+
+def calculos(valor_1, valor_2):
+  
+   soma= valor_1 + valor_2
+    
+   return soma
+    
+ 
+  
+n1= int(input("Digite um numero: ")) 
+
+print()
+
+n2= int (input("Digite um numero: "))
+
+print()
+
+
+
+resultado= calculos(n1,n2)
+
+
+
+print (f"O resultado é {resultado} ")
