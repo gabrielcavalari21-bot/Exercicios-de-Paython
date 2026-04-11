f85d2a68601f220a554308b0865d7011426d2e72
Gabriel cavalari dos Santos <gabrielcavalari21@gmail.com>
2026-04-11 07:08:03 -0300

    Exercício 25
    Verifique se uma palavra é um palíndromo (se é a mesma palavra lida de trás para frente).

---
+++ Exercicio 25 .py
@@ -0,0 +1,23 @@
+
+palavras= str(input(" Escreva uma palavra: "))
+
+
+
+palavra_invertida= palavras[::-1]
+
+
+
+if palavra_invertida==palavras:
+    
+    
+
+   print ("Ela é uma palavra polidromo.")
+    
+
+
+
+else: 
+
+   
+
+   print ("Ela não é uma palavra polidromo.")
